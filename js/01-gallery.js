4edaed9ef6ae68596742f0d98c26f5ebb11b5427
Champion-AS <108135994+Champion-AS@users.noreply.github.com>
2022-09-09 18:59:53 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+const markup = createMark(galleryItems)
+
+galleryEl.insertAdjacentHTML('beforeend', markup)
+
+
+function createMark(items) {
+    return items.map(item =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`).join('')
+}
