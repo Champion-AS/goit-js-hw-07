1f20ca75b314dce05af758b49eef98006896354b
Champion-AS <108135994+Champion-AS@users.noreply.github.com>
2022-09-12 23:40:18 +0300

    2+

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector ('.gallery')
-const picttures = galleryItems.map ()
 console.log(galleryItems);
+
+
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+
+const markup = createMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+
+function createMarkup(items) {
+  return items
+    .map((item) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+// подключам лайф-бокс
+const lightbox = new SimpleLightbox(".gallery a", {  
+  captionsData: "alt",
+  captionDelay: 250,
+});
+console.log(lightbox);
